159feceed4a37a7ec3a4a3a775778480016aab8a
ajd2144 <andrew.damico@northwestern.edu>
2018-07-17 08:37:36 -0500

    Added a header to each page and some ribbion styling. Added images and more content to the about me page.

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -30,77 +30,79 @@ class Education extends Component{
       let project = projects[this.state.selectedProject];
 
       return(
-         [<div className="row text-center align-items-cetner">
-            <div className="col-12 col-md-4 flex-2">
-               <h5>March 2018</h5>
+         <div className="container-fluid main">
+            <h1>Education</h1>
+            <div className="row text-center align-items-cetner">
+               <div className="col-12 col-md-4 flex-2">
+                  <h5>March 2018</h5>
+               </div>
+               <div className="col-12 col-md-4 flex-1">
+                  <img src={require(`../../resources/images/logos/nwcb-logo-horizontal.svg`)} alt="Northwestern Coding Boot Camp logo" />
+               </div>
+               <div className="col-12 col-md-4 flex-3">
+                  <h5>Evanston, IL</h5>
+               </div>
             </div>
-            <div className="col-12 col-md-4 flex-1">
-               <img src={require(`../../resources/images/logos/nwcb-logo-horizontal.svg`)} alt="Northwestern Coding Boot Camp logo" />
-            </div>
-            <div className="col-12 col-md-4 flex-3">
-               <h5>Evanston, IL</h5>
-            </div>
-         </div>,
-         <div>
-            <h4>Trilogy Coding Bootcamp - Full-stack Web Developer</h4>
-            <p>An intensive 28-week long boot camp designed to teach all levels of experience programmers on the skills needed to be able to create full-stack web applications. Skills learned consist of HTML5, CSS3, JavaScript, jQuery, Handlebars JS, Express server, MySQL, MongoDB, Firebase, and React JS.</p>
-            <h5>Final Application Built</h5>
-            <div id="accordion">
-               <div className="card-header">
-                  <div className="row d-flex">
-                     <div className="col-12 col-md-3">
-                        <div className="card h-100" onClick={this.toggleShow}>
-                           <img src={require(`../../resources/images/projects/jamBids/logo.png`)} alt="JAM Bids logo" />
+            <div>
+               <h4>Trilogy Coding Bootcamp - Full-stack Web Developer</h4>
+               <p>An intensive 28-week long boot camp designed to teach all levels of experience programmers on the skills needed to be able to create full-stack web applications. Skills learned consist of HTML5, CSS3, JavaScript, jQuery, Handlebars JS, Express server, MySQL, MongoDB, Firebase, and React JS.</p>
+               <h5>Final Application Built</h5>
+               <div id="accordion">
+                  <div className="card-header">
+                     <div className="row d-flex">
+                        <div className="col-12 col-md-3">
+                           <div className="card h-100" onClick={this.toggleShow}>
+                              <img src={require(`../../resources/images/projects/jamBids/logo.png`)} alt="JAM Bids logo" />
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div className={this.state.showProject ? "collapse show" : "collapse"}>
+                     <div className="card-body">
+                        <div className="row d-flex justify-content-center">
+                           <Carousel className="carousel-size" infiniteLoop={true} autoPlay={true} emulateTouch={true}>
+                              {project.images.map((e) => 
+                                 <div>
+                                    <img src={require(`../../resources/images/projects/${e.path}`)} alt={e.alt}/>
+                                    <p className="legend">{e.legend}</p>
+                                 </div>
+                              )}
+                           </Carousel>
+                        </div>
+                        <div className="row">
+                           <p>A modern designed real-time application where users can sign up and place bids or put items up for auction. The application uses web sockets through React JS and Express while being responsive for an enjoyable experience for both desktop and mobile.</p>
+                           <p>Lead devloper of a team of 3.</p>
                         </div>
                      </div>
                   </div>
                </div>
-               <div className={this.state.showProject ? "collapse show" : "collapse"}>
-                  <div className="card-body">
-                     <div className="row d-flex justify-content-center">
-                        <Carousel className="carousel-size" infiniteLoop={true} autoPlay={true} emulateTouch={true}>
-                           {project.images.map((e) => 
-                              <div>
-                                 <img src={require(`../../resources/images/projects/${e.path}`)} alt={e.alt}/>
-                                 <p className="legend">{e.legend}</p>
-                              </div>
-                           )}
-                        </Carousel>
-                     </div>
-                     <div className="row">
-                        <p>A modern designed real-time application where users can sign up and place bids or put items up for auction. The application uses web sockets through React JS and Express while being responsive for an enjoyable experience for both desktop and mobile.</p>
-                        <p>Lead devloper of a team of 3.</p>
-                     </div>
-                  </div>
+            </div>
+            <hr />
+            <div className="row text-center align-items-center">
+               <div className="col-12 col-md-4 flex-2">
+                  <h5>October 2014</h5>
+               </div>
+               <div className="col-12 col-md-4 flex-1">
+               <img src={require(`../../resources/images/logos/devry.png`)} alt="DeVry University logo" />
+               </div>
+               <div className="col-12 col-md-4 flex-3">
+                  <h5>Addison, IL</h5>
                </div>
             </div>
-         </div>,
-         <hr />,
-         <div className="row text-center align-items-center">
-            <div className="col-12 col-md-4 flex-2">
-               <h5>October 2014</h5>
+            <div>
+               <p>Bachelor of Science in Computer Information Systems - Database Management</p>
+               <h5>GPA: 3.89/4.0</h5>
+               <h5>Dean's List</h5>
+               <ul>
+                  <li>Fall 2014 semester</li>
+                  <li>Spring 2014 semester</li>
+                  <li>Winter 2013 semester</li>
+                  <li>Summer 2013 semester</li>
+                  <li>Spring 2013 semester</li>
+                  <li>Fall 2012 semester</li>
+               </ul>
             </div>
-            <div className="col-12 col-md-4 flex-1">
-            <img src={require(`../../resources/images/logos/devry.png`)} alt="DeVry University logo" />
-            </div>
-            <div className="col-12 col-md-4 flex-3">
-               <h5>Addison, IL</h5>
-            </div>
-         </div>,
-         <div>
-            <p>Bachelor of Science in Computer Information Systems - Database Management</p>
-            <h5>GPA: 3.89/4.0</h5>
-            <h5>Dean's List</h5>
-            <ul>
-               <li>Fall 2014 semester</li>
-               <li>Spring 2014 semester</li>
-               <li>Winter 2013 semester</li>
-               <li>Summer 2013 semester</li>
-               <li>Spring 2013 semester</li>
-               <li>Fall 2012 semester</li>
-            </ul>
-         </div>,
-         ]
+         </div>
       );
    }
 }
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -23,47 +23,50 @@ class Work extends Component{
 
    render(){
       return(
-         <div id="accordion">
-            <div className="card-header">
-               <div className="row d-flex accordin-background">
-                  <div className="col-3">
-                     <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
-                        <div className="card-body d-flex align-items-center justify-content-center" value="northwestern">
-                           <img className="accordin-logos" id="nwu-logo" value="northwestern" src={require(`../../resources/images/logos/northwestern_logo.png`)} alt="Northwestern Logo" />
+         <div className="container-fluid main">
+            <h1>Work Experience</h1>
+            <div id="accordion">
+               <div className="card-header">
+                  <div className="row d-flex accordin-background">
+                     <div className="col-3">
+                        <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
+                           <div className="card-body d-flex align-items-center justify-content-center" value="northwestern">
+                              <img className="accordin-logos" id="nwu-logo" value="northwestern" src={require(`../../resources/images/logos/northwestern_logo.png`)} alt="Northwestern Logo" />
+                           </div>
+                           <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="northwestern">January 2016 - Present</div>
                         </div>
-                        <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="northwestern">January 2016 - Present</div>
                      </div>
-                  </div>
-                  <div className="col-3">
-                     <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
-                        <div className="card-body d-flex align-items-center justify-content-center" value="sears">
-                           <img className="accordin-logos hide-logos" value="sears" src={require(`../../resources/images/logos/sears_holdings_logo.png`)} alt="Sears Holdings Logo" />
-                           <img className="accordin-logos hide-icons" value="sears" src={require(`../../resources/images/logos/sears6.jpg`)} alt="Sears Holdings Logo" />
+                     <div className="col-3">
+                        <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
+                           <div className="card-body d-flex align-items-center justify-content-center" value="sears">
+                              <img className="accordin-logos hide-logos" value="sears" src={require(`../../resources/images/logos/sears_holdings_logo.png`)} alt="Sears Holdings Logo" />
+                              <img className="accordin-logos hide-icons" value="sears" src={require(`../../resources/images/logos/sears6.jpg`)} alt="Sears Holdings Logo" />
+                           </div>
+                           <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="sears">Novermber 2014 - January 2016</div>
                         </div>
-                        <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="sears">Novermber 2014 - January 2016</div>
                      </div>
-                  </div>
-                  <div className="col-3">
-                     <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
-                        <div className="card-body d-flex align-items-center justify-content-center" value="devryeducationalgroup">
-                           <img className="accordin-logos hide-logos" id="devry-logo" value="devryeducationalgroup" src={require(`../../resources/images/logos/devry-education-group.png`)} alt="Sears Holdings Logo" />
-                           <img className="accordin-logos hide-icons" id="devry-logo" value="devryeducationalgroup" src={require(`../../resources/images/logos/devry-icon.png`)} alt="Sears Holdings Logo" />
+                     <div className="col-3">
+                        <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
+                           <div className="card-body d-flex align-items-center justify-content-center" value="devryeducationalgroup">
+                              <img className="accordin-logos hide-logos" id="devry-logo" value="devryeducationalgroup" src={require(`../../resources/images/logos/devry-education-group.png`)} alt="Sears Holdings Logo" />
+                              <img className="accordin-logos hide-icons" id="devry-logo" value="devryeducationalgroup" src={require(`../../resources/images/logos/devry-icon.png`)} alt="Sears Holdings Logo" />
+                           </div>
+                           <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="devryeducationalgroup">January 2014 - April 2014</div>
                         </div>
-                        <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="devryeducationalgroup">January 2014 - April 2014</div>
                      </div>
-                  </div>
-                  <div className="col-3">
-                     <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
-                        <div className="card-body d-flex align-items-center justify-content-center" value="stpeter">
-                           <img className="accordin-logos hide-logos" value="stpeter" src={require(`../../resources/images/logos/stpeter-logo.png`)} alt="St. Peter Church Logo" />
-                           <img className="accordin-logos hide-icons" value="stpeter" src={require(`../../resources/images/logos/st-peter-icon.png`)} alt="St. Peter Church Logo" />
+                     <div className="col-3">
+                        <div className="card h-100 accordin-header-transparent" onClick={this.selectJob}>
+                           <div className="card-body d-flex align-items-center justify-content-center" value="stpeter">
+                              <img className="accordin-logos hide-logos" value="stpeter" src={require(`../../resources/images/logos/stpeter-logo.png`)} alt="St. Peter Church Logo" />
+                              <img className="accordin-logos hide-icons" value="stpeter" src={require(`../../resources/images/logos/st-peter-icon.png`)} alt="St. Peter Church Logo" />
+                           </div>
+                           <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="stpeter">August 2012 - January 2014</div>
                         </div>
-                        <div className="card-body d-flex align-items-end justify-content-center timeframe hide-logos" value="stpeter">August 2012 - January 2014</div>
                      </div>
                   </div>
                </div>
+               <SelectedJob job={this.state.jobSelected} />
             </div>
-            <SelectedJob job={this.state.jobSelected} />
          </div>
       );
    }
